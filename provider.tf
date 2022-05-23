5b1ecf95b989a4659e5d59960dd7d9532744f683
D Souza <pramod-rolan.d-souza@capgemini.com>
2022-05-23 13:58:42 +0530

    added

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+  version         = "=3.0.0"
+  subscription_id = "9367c51c-2e2c-4780-8cd4-12eea748e6eb"
+  client_id       = "a210cffb-2d4c-46f1-a2f0-affb1aee3293"
+  client_secret   = "THt8Q~NHO_6NBg.USkzVoy4l5EpxXri5jvf2zavQ"
+  tenant_id       = "57df6259-4027-48b1-91c8-c5e82868ee0a"
+}
